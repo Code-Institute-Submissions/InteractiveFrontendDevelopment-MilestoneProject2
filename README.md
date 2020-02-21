babd0f1a52683f93e59a293e75bcc87d154b2474
Ev <evmur@protonmail.com>
2020-02-21 05:56:50 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# So here's the story.
+
+## This is a nutrition tracker app. It helps you to keep track of all the nutrients you are consuming on a daily basis.
---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+
+function saveNote() {
+
+    var markupStr = $('#summernote').summernote('code');
+
+    console.log(markupStr)
+
+}
+
+
+window.customElements.define('file-bag', class extends HTMLElement {
+    constructor() {
+      super();
+      var shadowRoot = this.attachShadow({mode: 'open'});
+      shadowRoot.innerHTML = `<strong>Shadow dom super powers for the win!</strong>`;
+    }
+  });
